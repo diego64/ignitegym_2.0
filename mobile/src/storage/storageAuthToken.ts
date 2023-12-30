bc84c17924fda9106a762156e1dda67e1b8e007f
diego64 <diegoferreira1964@gmail.com>
2023-12-30 18:55:48 -0300

    Configurando o refresh token

--- mobile/src/storage/storageAuthToken.ts
+++ mobile/src/storage/storageAuthToken.ts
@@ -2,16 +2,23 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { AUTH_STORAGE } from '@storage/storageConfig';
 
+type StorageAuthTokenProps = {
+  token: string;
+  refresh_token: string;
+}
+
 //Saving the User Token
-export async function storageAuthTokenSave(token: string) {
-  await AsyncStorage.setItem(AUTH_STORAGE, token);
+export async function storageAuthTokenSave({token, refresh_token}: StorageAuthTokenProps) {
+  await AsyncStorage.setItem(AUTH_STORAGE, JSON.stringify({ token, refresh_token }));
 }
 
 //Search for the user's Token on the device
 export async function storageAuthTokenGet() {
-  const token = await AsyncStorage.getItem(AUTH_STORAGE);
+  const response = await AsyncStorage.getItem(AUTH_STORAGE);
 
-  return token;
+  const { token, refresh_token }: StorageAuthTokenProps = response ? JSON.parse(response) : {}
+
+  return { token, refresh_token };
 }
 
 //Removing the Token from the device
